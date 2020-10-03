95bd97d62f44858f1c3f569a1f85f65ca6205e84
Dor <dor.biztrizer@gmail.com>
2020-10-03 17:14:33 +0300

    V.8

--- js/plugins.js
+++ js/plugins.js
@@ -21,7 +21,7 @@ $('.navbar-toggler').click(function () { //when navbar-toggler is clicked
 });
 
 
-/*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK ==========*/
+/*========== CLOSE MOBILE MENU ON CLICK  ==========*/
 $(document).ready(function () {
     $(document).click(function (event) {
         var clickover = $(event.target);
@@ -31,6 +31,44 @@ $(document).ready(function () {
         }
     });
 });
+
+
+
+/*========== SMOOTH SCROLLING TO LINKS ==========*/
+
+$(document).ready(function(){ 
+    $("a").on('click', function(event) {//click on any link;anchor tag;
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") { //for e.g. website.com#home - #home
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+        //console.log('hash:',hash)
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({ //animate whole html and body elements
+          scrollTop: $(hash).offset().top //scroll to the element with that hash
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash; //website.com - website.com#home
+          //Optional remove "window.location.hash = hash;" to prevent transparent navbar on load
+        });
+      } // End if
+    });
+  });
+
+
+
+
+
+
+
+
 
 /*========== BOUNCING DOWN ARROW ==========*/
 $(document).ready(function () {
